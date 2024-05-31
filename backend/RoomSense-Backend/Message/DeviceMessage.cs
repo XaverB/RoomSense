dad94dfd4b4d86233e63a38644edd28620e2062d
Buttinger Xaver <xaver.buttinger@gmail.com>
2024-05-31 12:12:56 +0200

    DeviceMessage

--- backend/RoomSense-Backend/Message/DeviceMessage.cs
+++ backend/RoomSense-Backend/Message/DeviceMessage.cs
@@ -4,10 +4,24 @@ namespace RoomSense_Backend.Message
 {
     public class DeviceMessage
     {
-        public required string DeviceId { get; set; }
         public required string DeviceType { get; set; }
         public required string DeviceLocation { get; set; }
-        public required string Value { get; set; }
-        public required string Timestamp { get; set; }
+
+        private readonly Payload Payload;
+
+        public DeviceMessage(Payload payload, string DeviceLocation)
+        {
+            if (string.IsNullOrEmpty(DeviceLocation))
+            {
+                throw new ArgumentException($"'{nameof(DeviceLocation)}' cannot be null or empty.", nameof(DeviceLocation));
+            }
+
+            Payload = payload ?? throw new ArgumentNullException(nameof(payload));
+            this.DeviceLocation = DeviceLocation;
+        }
+
+        public  string DeviceId => Payload.DeviceId;
+        public  string Value => Payload.DeviceId;
+        public  string Timestamp => Payload.DeviceId;
     }
 }
--- backend/RoomSense-Backend/Message/Payload.cs
+++ backend/RoomSense-Backend/Message/Payload.cs
@@ -1,8 +1,11 @@
-﻿namespace RoomSense_Backend.Message
+﻿// Ignore Spelling: Timestamp
+
+namespace RoomSense_Backend.Message
 {
     public class Payload
     {
         public required string DeviceId { get; set; }
         public required string Value { get; set; }
+        public required string Timestamp { get; set; }
     }
 }
